772267aca14f22711d37561db99e5510ea446819
nykevinwong <nykevinwong@users.noreply.github.com>
2020-05-29 17:28:16 -0400

    Update UnionFind.java

--- algorithm-fundamental/UnionFind.java
+++ algorithm-fundamental/UnionFind.java
@@ -1,3 +1,5 @@
+// https://labuladong.gitbook.io/algo/gao-pin-mian-shi-xi-lie/unionfind-suan-fa-ying-yong
+
 class UnionFind {
     // 记录连通分量个数
     private int count;
@@ -55,4 +57,37 @@ class UnionFind {
     public int count() {
         return count;
     }
+}
+
+public class Test
+{
+public boolean equationsPossible(String[] equations) {
+    // 26 个英文字母
+    UF uf = new UF(26);
+    // 先让相等的字母形成连通分量
+    for (String eq : equations) {
+        if (eq.charAt(1) == '=') {
+            char x = eq.charAt(0);
+            char y = eq.charAt(3);
+            uf.union(x - 'a', y - 'a');
+        }
+    }
+    // 检查不等关系是否打破相等关系的连通性
+    for (String eq : equations) {
+        if (eq.charAt(1) == '!') {
+            char x = eq.charAt(0);
+            char y = eq.charAt(3);
+            // 如果相等关系成立，就是逻辑冲突
+            if (uf.connected(x - 'a', y - 'a'))
+                return false;
+        }
+    }
+    return true;
+}
+    
+public static void main(String[] args)
+{
+    
+}
+    
 }
